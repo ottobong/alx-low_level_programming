65c6a0c8409f3faed83b37875e2ce26599ed57a0
Ottobong <clemency50@gmail.com>
2022-04-20 23:38:27 +0100

    holberton and main update

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,11 +3,11 @@
  ​* reset_to_98 - reset argument to 98
  * @n: pointer
  * Return: void
-*/
+ */
 
- ​void​ ​reset_to_98​(​int​ *n)
- ​{
+void​ ​reset_to_98​(​int​ *n)
+{
 
- ​        *n = ​98​;
+*n = ​98​;
 
- ​}
+}
