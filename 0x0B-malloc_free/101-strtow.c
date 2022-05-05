76ac32151e78755aeef426585fab81cc83b92890
ottobong <clemency50@gmail.com>
2022-05-05 14:52:25 -0700

    Added new updates

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -13,7 +13,7 @@ char **split;
 int i, j = 0, temp = 0, size = 0, words = num_words(str);
 
 if (words == 0)
-return (NULL)
+return (NULL);
 split = (char **) malloc(sizeof(char *) * (words + 1));
 if (split != NULL)
 {
